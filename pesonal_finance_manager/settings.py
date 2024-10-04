e7aa7fb4471ea38bfb6526234634c6c50a77cb1b
HydrallHarsh <harshsantwani.cse21@aii.ac.in>
2024-10-04 23:54:41 +0530

    Changed env variables and settings

--- pesonal_finance_manager/settings.py
+++ pesonal_finance_manager/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG','False').lower() == 'true'
 
-ALLOWED_HOSTS = ["https://moneta-personal-finance-website.onrender.com"]
+ALLOWED_HOSTS = ["moneta-personal-finance-website.onrender.com"]
 
 
 # Application definition
