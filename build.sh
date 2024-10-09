ce9a7d92dbf27b8e0a26a8ca20d25c808f09909a
HydrallHarsh <harshsantwani.cse21@aii.ac.in>
2024-10-09 23:44:25 +0530

    Edited .sh

--- build.sh
+++ build.sh
@@ -5,15 +5,15 @@ if ! command -v pip &> /dev/null
 then
     echo "Installing pip"
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python3.9 get-pip.py
+    python3.11.4 get-pip.py
 fi
 
 # Install project dependencies
-python3.9 -m pip install -r requirements.txt
+python3.11.4 -m pip install -r requirements.txt
 
 echo "Make migrations"
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate
+python3.11.4 manage.py makemigrations
+python3.11.4 manage.py migrate
 
 echo "Collect static files"
-python3.9 manage.py collectstatic --noinput --clear
+python3.11.4 manage.py collectstatic --noinput --clear
