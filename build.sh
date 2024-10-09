5ee6bfee2d486ad768ba181c91e7de8a7de44045
HydrallHarsh <harshsantwani.cse21@aii.ac.in>
2024-10-09 23:37:33 +0530

    Edited .sh

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
 echo "Building the project"
 
-python3.9 -m pip install -r requirements.txt
+# Install pip if not installed
+if ! command -v pip &> /dev/null
+then
+    echo "Installing pip"
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python3.9 get-pip.py
+fi
 
+# Install project dependencies
+python3.9 -m pip install -r requirements.txt
 
 echo "Make migrations"
 python3.9 manage.py makemigrations
-python manage.py migrate
+python3.9 manage.py migrate
 
 echo "Collect static files"
 python3.9 manage.py collectstatic --noinput --clear
