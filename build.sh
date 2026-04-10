c484935696803e49cb2ac790f45e7fc1f6fdc24d
HydrallHarsh <harshsantwani.cse21@aii.ac.in>
2026-04-10 20:50:44 +0530

    refactor: simplify build script by removing pip installation check and makemigrations step

--- build.sh
+++ build.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
+set -euo pipefail
 
 echo "Building the project"
 
-# Install pip if not installed
-if ! command -v pip &> /dev/null
-then
-    echo "Installing pip"
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python3 get-pip.py
-    rm get-pip.py  # Clean up
-fi
-
 # Install project dependencies
+python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 
-echo "Make migrations"
-python3 manage.py makemigrations
+echo "Running migrations"
 python3 manage.py migrate
 
 echo "Collect static files"
